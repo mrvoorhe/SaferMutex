a8cc62d6aa39de82e0c383b789274a2094d21100
Mike Voorhees <michaelv@unity3d.com>
2017-03-03 16:16:58 -0500

    Added a second version of FileBased that doesn't use

---
+++ SaferMutex.Tests/FileBased/ThreadedMutexTestsV2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NiceIO;
+using NUnit.Framework;
+using SaferMutex.Tests.BaseSuites;
+
+namespace SaferMutex.Tests.FileBased
+{
+    [TestFixture]
+    public class ThreadedMutexTestsV2 : BaseThreadedTests
+    {
+        private static readonly NPath _tempDirectory = NPath.SystemTemp.Combine("SaferMutex");
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _tempDirectory.DeleteContents();
+        }
+
+        protected override ISaferMutexMutex CreateMutexImplementation(bool initiallyOwned, string name, out bool owned, out bool createdNew)
+        {
+            return new SaferMutex.FileBased2(initiallyOwned, name, Scope.CurrentProcess, out owned, out createdNew, _tempDirectory.ToString());
+        }
+    }
+}
--- SaferMutex/SaferMutex.cs
+++ SaferMutex/SaferMutex.cs
@@ -611,6 +611,354 @@ namespace SaferMutex
             }
         }
 
+        internal sealed class FileBased2 : ISaferMutexMutex
+        {
+            private const string LockFileExtension = "lock";
+            private const int WaitPeriodDuringInfiniteTimeout = 100;
+
+            private const int AttemptsPerTimeout = 10;
+
+            private readonly string _lockFilePath;
+            private readonly bool _createdNew;
+            private FileStream _lockFileStream;
+            private bool _hasLock;
+
+            private bool _disposed;
+
+            public FileBased2(bool initiallyOwned, string name, Scope scope, out bool owned, out bool createdNew, string customRootLockDirectory = null)
+            {
+                if (name.StartsWith("Global\\"))
+                    throw new ArgumentException($"The Global\\ name prefix should not be used.  Use the Scope parameter to control the scope of the mutex");
+
+                _lockFilePath = GenerateLockFilePath(name, scope, customRootLockDirectory);
+
+                if (!initiallyOwned)
+                {
+                    TryTouchLockFile(out createdNew);
+                    owned = false;
+                    _createdNew = createdNew;
+                    return;
+                }
+
+                if (!TryLockNew(out createdNew))
+                {
+                    owned = false;
+                    _createdNew = createdNew;
+                    return;
+                }
+
+                owned = true;
+                //owned = TryLock();
+                _createdNew = createdNew;
+            }
+
+            public void WaitOne()
+            {
+                WaitOne(null);
+            }
+
+            public bool WaitOne(int millisecondsTimeout)
+            {
+                return WaitOne(null, millisecondsTimeout);
+            }
+
+            public bool WaitOne(WaitHandle breakHandle)
+            {
+                return WaitOne(breakHandle, Timeout.Infinite);
+            }
+
+            public bool WaitOne(WaitHandle breakHandle, int millisecondsTimeout)
+            {
+                int notUsed;
+                return WaitOne(breakHandle == null ? null : new[] { breakHandle }, millisecondsTimeout, out notUsed);
+            }
+
+            public bool WaitOne(WaitHandle[] breakHandles, out int index)
+            {
+                return WaitOne(breakHandles, Timeout.Infinite, out index);
+            }
+
+            public bool WaitOne(WaitHandle[] breakHandles, int millisecondsTimeout, out int index)
+            {
+                // If the timeout is 0, then we will just check and return immediately.
+                if (millisecondsTimeout == 0)
+                {
+                    if (TryLock())
+                    {
+                        index = int.MinValue;
+                        return true;
+                    }
+
+                    index = -1;
+                    return false;
+                }
+
+                int sleepTimeBetweenTries = WaitPeriodDuringInfiniteTimeout;
+                if (millisecondsTimeout != Timeout.Infinite)
+                {
+                    // Make sure the sleep time is at least 1 ms. (not 0).
+                    sleepTimeBetweenTries = Math.Max(millisecondsTimeout / AttemptsPerTimeout, 1);
+
+                    // Make sure that a long timeout doesn't mean we wont check very often.  The InfiniteTimeout period should
+                    // be the longest that any mutex ever waits to check.
+                    sleepTimeBetweenTries = Math.Min(sleepTimeBetweenTries, WaitPeriodDuringInfiniteTimeout);
+                }
+
+                int elapsedTimeCounter = 0;
+                while (true)
+                {
+                    if (TryLock())
+                    {
+                        index = int.MinValue;
+                        return true;
+                    }
+
+                    // Do stuff for timeout break condition
+                    if (millisecondsTimeout != Timeout.Infinite)
+                    {
+                        elapsedTimeCounter += sleepTimeBetweenTries;
+
+                        if (elapsedTimeCounter >= millisecondsTimeout)
+                        {
+                            index = -1;
+                            return false;
+                        }
+                    }
+
+                    if (breakHandles != null)
+                    {
+                        // While we sleep, wait on our break handless
+                        int breakIndex = WaitHandle.WaitAny(breakHandles, sleepTimeBetweenTries);
+
+                        if (breakIndex != WaitHandle.WaitTimeout)
+                        {
+                            index = breakIndex;
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        Thread.Sleep(sleepTimeBetweenTries);
+                    }
+                }
+            }
+
+            public void Dispose()
+            {
+                if (_disposed)
+                    return;
+
+                ReleaseMutex();
+
+                if (_lockFileStream != null)
+                {
+                    _lockFileStream.Dispose();
+                    _lockFileStream = null;
+                }
+
+                TryDeleteLock();
+
+                this._disposed = true;
+            }
+
+            public void ReleaseMutex()
+            {
+                if (_hasLock == false)
+                    return;
+
+                try
+                {
+                    _lockFileStream.Dispose();
+                    _lockFileStream = null;
+                    _hasLock = false;
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            private bool TryLock()
+            {
+                if (_lockFileStream != null)
+                {
+                    throw new InvalidOperationException("Already have ownership of the mutex");
+                }
+
+                try
+                {
+                    _lockFileStream = new FileStream(this._lockFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+                    _hasLock = true;
+                }
+                catch (IOException)
+                {
+                    return false;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    // Can happen instead of IOException if another user is holding the file
+                    return false;
+                }
+                catch (System.Security.SecurityException)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            private bool TryLockNew(out bool createdNew)
+            {
+                var lockDirectory = Path.GetDirectoryName(_lockFilePath);
+                if (!Directory.Exists(lockDirectory))
+                {
+                    // Robust support for race free directory creation is not implemented.
+                    // And isn't an issue for the time being since we will use a root temp dir that we can expect to exist
+                    // ...absent a user manually deleting it
+                    throw new DirectoryNotFoundException($"The lock directory must already exist : {lockDirectory}");
+                }
+
+                if (_lockFileStream != null)
+                {
+                    throw new InvalidOperationException("Already have ownership of the mutex");
+                }
+
+                try
+                {
+                    // TODO by Mike : Probably not a great idea.  Could this be racey?  Might be OK since the stream creation would throw and then createdNew would be set back to false.
+                    createdNew = !File.Exists(_lockFilePath);
+
+                    _lockFileStream = new FileStream(this._lockFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+                    _hasLock = true;
+                }
+                catch (IOException)
+                {
+                    createdNew = false;
+                    return false;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    createdNew = false;
+                    // Can happen instead of IOException if another user is holding the file
+                    return false;
+                }
+                catch (System.Security.SecurityException)
+                {
+                    createdNew = false;
+                    return false;
+                }
+
+                return true;
+            }
+
+            private void TryTouchLockFile(out bool createdNew)
+            {
+                var lockDirectory = Path.GetDirectoryName(_lockFilePath);
+                if (!Directory.Exists(lockDirectory))
+                {
+                    // Robust support for race free directory creation is not implemented.
+                    // And isn't an issue for the time being since we will use a root temp dir that we can expect to exist
+                    // ...absent a user manually deleting it
+                    throw new DirectoryNotFoundException($"The lock directory must already exist : {lockDirectory}");
+                }
+
+                try
+                {
+                    //// TODO by Mike : Probably not a great idea.  Could this be racey?  Might be OK since the stream creation would throw and then createdNew would be set back to false.
+                    //if (File.Exists(_lockFilePath))
+                    //{
+                    //    createdNew = false;
+                    //    return;
+                    //}
+
+                    //createdNew = !File.Exists(_lockFilePath);
+
+                    using (var tmpStream = new FileStream(_lockFilePath, FileMode.CreateNew, FileAccess.ReadWrite, FileShare.None))
+                    {
+                        //if (_scope == Scope.SystemWide)
+                        //{
+                        //    // Woud need  (666) rw-rw-rw- permissions on the lock file
+                        //    throw new NotImplementedException("To support this, would need to chmod 011 on the lock file");
+                        //}
+                    }
+
+                    createdNew = true;
+                }
+                catch (IOException)
+                {
+                    createdNew = false;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    createdNew = false;
+                }
+            }
+
+            private void TryDeleteLock()
+            {
+                // Only let the original creator do the file clean.  This way we can accurately track "createdNew"
+                if (!_createdNew)
+                    return;
+
+                try
+                {
+                    File.Delete(this._lockFilePath);
+                }
+                catch (IOException)
+                {
+                    //  Another processes mutex class could have grabbed ownership over the lock file between the time
+                    // this instance closed it's write stream and the time this delete call happens
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    // It's possible that this mutex is used by two different processes running as different users.  When this happens
+                    // we may get an auth exception when deleting the file instead of an IOException
+                }
+            }
+
+            private static string SanitizeForPathUsage(string name)
+            {
+                return name.Replace(' ', '_');
+            }
+
+            private static string LockDirectoryFor(Scope scope, string customRootLockDirectory)
+            {
+                if (!string.IsNullOrEmpty(customRootLockDirectory))
+                    return customRootLockDirectory;
+
+                switch (scope)
+                {
+                    case Scope.CurrentProcess:
+                    case Scope.CurrentUser:
+                        // For now, use the root temp dir in order to avoid directory creation races
+                        return Path.GetTempPath();
+                    //case Scope.SystemWide:
+                    //    throw new NotImplementedException();
+                    default:
+                        throw new NotImplementedException($"Support for scope {scope} has not been implemented");
+                }
+            }
+
+            private static string LockFileNameFor(string pathSafeName, Scope scope)
+            {
+                switch (scope)
+                {
+                    case Scope.CurrentProcess:
+                        return $"BetterMutex_{System.Diagnostics.Process.GetCurrentProcess().Id}_{pathSafeName}.{LockFileExtension}";
+                    case Scope.CurrentUser:
+                        //case Scope.SystemWide:
+                        return $"BetterMutex_{pathSafeName}.{LockFileExtension}";
+                    default:
+                        throw new NotImplementedException($"Support for scope {scope} has not been implemented");
+                }
+            }
+
+            private static string GenerateLockFilePath(string name, Scope scope, string customRootLockDirectory)
+            {
+                var fileName = LockFileNameFor(SanitizeForPathUsage(name), scope);
+                return Path.Combine(LockDirectoryFor(scope, customRootLockDirectory), fileName);
+            }
+        }
+
         #endregion
     }
 }
